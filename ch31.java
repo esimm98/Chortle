bb83277627ac9e989879e439eebad72d5771e62b
esimm98 <esimm98@gmail.com>
2016-01-31 22:41:22 -0700

    31 1 and 2 complete

--- ch31.java
+++ ch31.java
@@ -2,5 +2,62 @@ import java.io.*;
 import java.util.Scanner;
 
 class Car {
-	
+	double startMiles;
+	double endMiles;
+	double gallons;
+
+	Car(double first) {
+		startMiles = first;
+	}
+
+	void fillUp(double miles, double gals) {
+		endMiles = miles;
+		gallons = gals;
+	}
+
+	double calculateMPG() {
+		return (endMiles - startMiles) / gallons;
+	}
+
+	boolean gasHog() {
+		if (calculateMPG() < 15.0) {
+			return true;
+		}
+		return false;
+	}
+
+	boolean economyCar() {
+		if (calculateMPG() > 30.0) {
+			return true;
+		}
+		return false;
+	}
+}
+
+class MPG {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double startMiles, endMiles, gallons;
+
+		System.out.print("New odometer reading: ");
+		startMiles = input.nextDouble();
+
+		Car car = new Car(startMiles);
+
+		System.out.println("Filling Station Visit ");
+		System.out.print("Odometer reading: ");
+		endMiles = input.nextDouble();
+		System.out.print("Gas filled up: ");
+		gallons = input.nextDouble();
+
+		car.fillUp(endMiles, gallons);
+		System.out.println("MPG: " + car.calculateMPG());
+
+		if (car.gasHog()) {
+			System.out.println("What a gas hog.");
+		}
+		if (car.economyCar()) {
+			System.out.println("Woah, an efficient car.");
+		}
+	}
 }

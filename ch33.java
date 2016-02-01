610a037fcdcb2cfa603f6fc39be9d16553b677c3
esimm98 <esimm98@gmail.com>
2016-01-31 23:14:10 -0700

    Ex3 Complete, 4 is strange

--- ch33.java
+++ ch33.java
@@ -30,7 +30,7 @@ class Box {
 	}
 
 	public double surfaceArea() {
-		return 2 * baseArea + 2 * sideArea * 2 * frontArea;
+		return 2 * baseArea() + 2 * sideArea() * 2 * frontArea();
 	}
 }
 
@@ -38,7 +38,68 @@ class BoxTester {
 	public static void main(String[] args) {
 		Box box = new Box(2.5, 5.0, 6.0);
 
-		System.out.println("Area: " + box.area() + " volume: " + box.volume());
+		System.out.println("Surface Area: " + box.surfaceArea() + " volume: " + box.volume());
 		System.out.println("length: " + box.length + " width: " + box.width + " height: " + box.height);
+	}
+}
+
+class Box3 {
+	public double width, height, length;
+
+	Box3 (double w, double h, double l) {
+		width = w;
+		height = h;
+		length = l;
+	}
+
+	Box3 (Box3 oldBox) {
+		width = oldBox.width;
+		height = oldBox.height;
+		length = oldBox.length;
+	}
+
+	public double volume() {
+		return width * height * length;
+	}
+
+	public double baseArea() {
+		return width * length;
+	}
+
+	public double sideArea() {
+		return height * width;
+	}
+
+	public double frontArea() {
+		return length * height;
+	}
+
+	public double surfaceArea() {
+		return 2 * baseArea() + 2 * sideArea() * 2 * frontArea();
+	}
+
+	public double length() {
+		return length;
+	}
+
+	public double width() {
+		return width;
+	}
+
+	public double height() {
+		return height;
+	}
+}
+
+class BoxTester3 {
+	public static void main(String[] args) {
+		Box3 box = new Box3(2.5, 5.0, 6.0);
+		Box3 box3 = new Box3(box);
+
+		System.out.println("Surface Area: " + box.surfaceArea() + " volume: " + box.volume());
+		System.out.println("length: " + box.length + " width: " + box.width + " height: " + box.height);
+
+		System.out.println("Surface Area: " + box3.surfaceArea() + " volume: " + box3.volume());
+		System.out.println("length: " + box3.length + " width: " + box3.width + " height: " + box3.height);
 	}
 }
